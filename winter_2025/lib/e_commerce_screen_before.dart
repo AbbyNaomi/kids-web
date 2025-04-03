84eee3e0f8870fd54ee470b87ef4f025859d296e
Yesui <138767044+AbbyNaomi@users.noreply.github.com>
2025-04-03 19:59:23 +0800

    add: hello project

--- winter_2025/lib/e_commerce_screen_before.dart
+++ winter_2025/lib/e_commerce_screen_before.dart
@@ -7,68 +7,204 @@ class ECommerceScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.purple,
-      appBar: AppBar(
-        backgroundColor: Colors.purpleAccent,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(bottom: Radius.circular(30))),
-        leading: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Icon(Icons.home),
-        ),
-        title: Text('Let\'s go shopping'),
-        elevation: 0,
-        actions: <Widget>[
-          Padding(padding: EdgeInsets.all(20.0),
-          child: Icon(Icons.shopping_cart),
-          ),
-        ],
-      ),
+      appBar: _buildAppBar(),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Column(
           children: <Widget>[
-            Row(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text(
-                    'Recommented',
-                    style: TextStyle(color: Colors.white,
-                      fontSize: 17,
-                      fontWeight: FontWeight.bold
+            _buildToggleBar(),
+            Image.asset('assets/images/woman_shopping.jpg'),
+            SizedBox(
+              height: 15,
+            ),
+            Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        height: 80,
+                        decoration: BoxDecoration(
+                            color: Colors.orangeAccent,
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Center(
+                          child: Text(
+                            'Best Sellers',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
                     ),
-                  ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Expanded(
+                      child: Container(
+                        height: 80,
+                        decoration: BoxDecoration(
+                            color: Colors.indigoAccent,
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Center(
+                          child: Text(
+                            'Daily Deals',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text(
-                    'Formal Wear',
-                    style: TextStyle(color: Colors.white,
-                        fontSize: 17,
-                        fontWeight: FontWeight.bold
-                    ),
-                  ),
+                SizedBox(
+                  height: 15,
                 ),
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text(
-                    'Casual Wear',
-                    style: TextStyle(color: Colors.white,
-                        fontSize: 17,
-                        fontWeight: FontWeight.bold
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        height: 80,
+                        decoration: BoxDecoration(
+                            color: Colors.lightGreen,
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Center(
+                            child: Text(
+                              'Must buy in summer',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ),
+                      ),
                     ),
-                  ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Expanded(
+                      child: Container(
+                        height: 80,
+                        decoration: BoxDecoration(
+                            color: Colors.redAccent,
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Center(
+                          child: Text(
+                            'Last Chance',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 15,
                 ),
               ],
             ),
-            Image.asset('assets/images/woman_shopping.jpg'),
-            SizedBox(height: 15,),
-            Row(
-              children: [],
-            )
+            Container(
+              height: 200,
+              color: Colors.white,
+              child: Row(
+                children: [
+                  Image.asset(
+                    'assets/images/textiles.jpg',
+                    fit: BoxFit.fitHeight,
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Lorem Ipsum',
+                            style: Theme.of(context).textTheme.headlineSmall,
+                          ),
+                          Text(
+                              'Dolot sit amet, consectetur adipiscing elit. Quisque faucubus.',style: TextStyle(
+                            fontSize: 20,
+
+                          ),),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
+    );
+  }
+
+  Row _buildToggleBar() {
+    return Row(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Recommented',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 17,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Formal Wear',
+                  style: TextStyle(
+                      color: Colors.white54,
+                      fontSize: 17,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Casual Wear',
+                  style: TextStyle(
+                      color: Colors.white54,
+                      fontSize: 17,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: Colors.purpleAccent,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(bottom: Radius.circular(30))),
+      leading: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Icon(Icons.home),
+      ),
+      title: Text('Let\'s go shopping'),
+      elevation: 0,
+      actions: <Widget>[
+        Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Icon(Icons.shopping_cart),
+        ),
+      ],
     );
   }
 }
